e4d128e410b80d27b7b986ccae84a916485c7270
Magno Santos <magno.ufvjm@gmail.com>
2019-01-07 17:17:04 -0200

    Adicao do Ioc para injeção de dependências, inicialização na classse Startup

--- 1 - Host/TestApiWatson/TestApiWatson/Startup.cs
+++ 1 - Host/TestApiWatson/TestApiWatson/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using TestApiWatson.Infra.CrossCutting.IoC;
 
 namespace TestApiWatson
 {
@@ -22,9 +24,15 @@ namespace TestApiWatson
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            IoCConfig.Inicializar();
+                       
+            IoCConfig.ConstrutorContainer.Populate(services);
+
+            return new AutofacServiceProvider(IoCConfig.ConstrutorContainer.Build());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ 4 - Infra/2 - CrossCutting/TestApiWatson.Infra.CrossCutting.IoC/IoCConfig.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace TestApiWatson.Infra.CrossCutting.IoC
+{
+    public class IoCConfig
+    {
+        public static ContainerBuilder ConstrutorContainer { get; set; }
+        public static IContainer Container { get; set; }
+        private static bool aplicacaoEmTesteUnitario;
+
+        public static void Inicializar()
+        {
+            ConstrutorContainer = new ContainerBuilder();
+
+            //DefinirAplicacaoEmTesteOuNao();
+
+            ConfigurarInjecoesServiceAgents();
+        }
+
+        public static void Build()
+        {
+            Container = ConstrutorContainer.Build();
+        }
+
+        //private static void DefinirAplicacaoEmTesteOuNao()
+        //{
+        //    var configuracoes = new ConfigurationBuilder()
+        //                            .AddJsonFile("appsettings.json", optional: false)
+        //                            .Build();
+        //    IConfigurationSection configurationSection = configuracoes.GetSection("AppConfiguration");
+        //    aplicacaoEmTesteUnitario = bool.Parse(configurationSection.GetSection("AplicacaoEmTesteUnitario").Value);
+        //}
+
+
+        private static void ConfigurarInjecoesServiceAgents()
+        {
+            //ConstrutorContainer.RegisterType<WatsonAgentMock>().As<IWatsonAgent>();
+        }       
+    }
+}
